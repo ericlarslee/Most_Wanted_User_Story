863ffe10d36fc11095e7aec7e0ebae3f80c1ff9f
ericlarslee <ericlarslee@gmail.com>
2021-03-12 11:44:53 -0600

    Added more criterion to searchBy function

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 'use strict';
 
-function searchByName(){
+function searchBy(){
     // Grabbing the values from our nameForm form and inputs.
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
+    let eyeColorInput = document.forms['nameForm']['eyeColor'].value;
+    let genderInput = document.forms['nameForm']['gender'].value;
+    let occupationInput = document.forms['nameForm']['occupation'].value;
 
+    
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
         if(person.firstName === firstNameInput && person.lastName === lastNameInput){
@@ -25,12 +29,10 @@ function searchByName(){
 
 function searchByEyeColor(){
     //Maybe will be using Eye color as a search value and seing how something would be return
- let eyeColorInput = document.forms['eyeForm']['fEye'].value;
- let filteredEyePeople = people.filter(function (person) {
      if(person.eyeColor === eyeColorInput) {
          return true;
      }
-     return false;
+    return false;
 });
 if(filteredEyePeople.length > 0){
     console.log(filteredEyePeople);

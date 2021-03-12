7132e42c64d228dbd7a22ae887e24d089ed5c1d7
ericlarslee <ericlarslee@gmail.com>
2021-03-12 14:12:40 -0600

    added filter functions for each of the categories that will be searchable

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ function searchBy(){
     let firstNameInput = document.forms['nameForm']['fname'].value;
     let lastNameInput = document.forms['nameForm']['lname'].value;
     let eyeColorInput = document.forms['nameForm']['eyeColor'].value;
-    let genderInput = document.forms['nameForm']['gender'].value;
+   /* let genderInput = document.forms['nameForm']['gender'].value;
     let occupationInput = document.forms['nameForm']['occupation'].value;
-
+ */
     
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
@@ -26,16 +26,48 @@ function searchBy(){
 }
 
 
-/* 
+ 
 function searchByEyeColor(){
-    
-     if(person.eyeColor === eyeColorInput) {
-         return true;
-     }
+    let eyeColorInput = document.forms['nameForm']['eyeColor'].value;
+    let filteredEyeColor = people.filter(function (person) {
+    if(person.eyeColor === eyeColorInput) {
+        return true;
+    }
     return false;
-});
-if(filteredEyePeople.length > 0){
-    console.log(filteredEyePeople);
-}else{
-    console.log('Sorry, looks like there is no one with that eye color.');
-} */
+    }); 
+    if(filteredEyeColor.length > 0){
+        console.log(filteredEyeColor);
+    }else{
+        console.log('Sorry, looks like there is no one with that eye color.');
+    }
+}
+
+function searchByGender(){
+    let eyeColorInput = document.forms['nameForm']['gender'].value;
+    let filteredGender = people.filter(function (person) {
+    if(person.gender === genderInput) {
+        return true;
+    }
+    return false;
+    }); 
+    if(filteredGender.length > 0){
+        console.log(filteredGender);
+    }else{
+        console.log('Sorry, looks like there is no one with that gender.');
+    }
+}
+
+function searchByOccupation(){
+    let occupationInput = document.forms['nameForm']['occupation'].value;
+    let filteredOccupation = people.filter(function (person) {
+    if(person.occupation === occupationInput) {
+        return true;
+    }
+    return false;
+    }); 
+    if(filteredOccupation.length > 0){
+        console.log(filteredOccupation);
+    }else{
+        console.log('Sorry, looks like there is no one with that occupation.');
+    }
+}
